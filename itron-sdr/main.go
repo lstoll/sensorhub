2f1830f1c7c4e6c30356a3d0339fb8fc3ef61a01
Lincoln Stoll <lincoln.stoll@gmail.com>
2015-10-19 00:46:59 -0400

    Optionally calculate and report spend

--- itron-sdr/main.go
+++ itron-sdr/main.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"strconv"
 	"time"
 
 	"github.com/kouhin/envflag"
@@ -16,6 +17,8 @@ import (
 var (
 	previousRead       int
 	consumptionCounter chan int64
+	costGauge          chan interface{}
+	gasPrice           float64
 )
 
 type MeterRead struct {
@@ -40,10 +43,17 @@ func main() {
 		libratoUser   = flag.String("librato-user", "REQUIRED", "User for Librato")
 		libratoToken  = flag.String("librato-token", "REQUIRED", "Token for Librato")
 		libratoSource = flag.String("librato-source", "REQUIRED", "Source name for librato")
+		gasPriceIn    = flag.String("gas-price", "OPTIONAL", "Price of gas Will report as additional metric")
 	)
 	if err := envflag.Parse(); err != nil {
 		panic(err)
 	}
+	gasPriceInFl, err := strconv.ParseFloat(*gasPriceIn, 64)
+	if err != nil {
+		fmt.Println("gas-price has to be a number")
+		os.Exit(1)
+	}
+	gasPrice = gasPriceInFl
 	if *meterId == "REQUIRED" {
 		fmt.Println("meter-id is a required field")
 		os.Exit(1)
@@ -64,7 +74,8 @@ func main() {
 	metrics := librato.NewSimpleMetrics(*libratoUser, *libratoToken, *libratoSource)
 	defer metrics.Wait()
 	defer metrics.Close()
-	consumptionCounter = metrics.GetCounter("gas.cf")
+	consumptionCounter = metrics.GetCounter("sensor.utility.gas.cf")
+	costGauge = metrics.GetGauge("sensor.utility.gas.spend")
 
 	cmdName := "rtlamr"
 	cmdArgs := []string{"-format=json", "-filterid=" + *meterId}
@@ -106,6 +117,16 @@ func main() {
 func processLine(read *MeterRead) {
 	fmt.Printf("at %s, consumption is %d", time.Now().Format("2006-01-02T15:04:05.999999-07:00"), read.Message.Consumption)
 	consumptionCounter <- int64(read.Message.Consumption)
+
+	if gasPrice != 0 && previousRead != 0 {
+		// we're tracking price, compare to the previous read and multiply by price
+		consumedSinceLast := read.Message.Consumption - previousRead
+		spentSinceLast := float64(consumedSinceLast) * (gasPrice / 1000)
+		fmt.Printf(" and amount spent since last read is %f", spentSinceLast)
+		costGauge <- spentSinceLast
+	}
+
+	fmt.Println("")
 
 	previousRead = read.Message.Consumption
 }
